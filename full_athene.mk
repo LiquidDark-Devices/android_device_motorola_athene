724ca84ebb98ed133f43f26598476d7f93d31866
LiquidDark <fisch890@gmail.com>
2017-02-19 15:14:37 +0100

    Update full_athene.mk

--- full_athene.mk
+++ full_athene.mk
@@ -24,3 +24,4 @@ $(call inherit-product, device/motorola/athene/device.mk)
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := athene
 PRODUCT_NAME := full_athene
+PRODUCT_MODEL := Moto G4 Plus
