d627330f4210d5b727a1d436413eaa9f7d99fc4b
t-rex600 <fisch890@gmail.com>
2017-06-04 09:24:55 +0200

    LDify

--- liquid.mk
+++ liquid.mk
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/xenonhd/config/common_full_phone.mk)
+# Inherit some common LiquidDark stuff.
+$(call inherit-product, vendor/liquid/config/common_full_phone.mk)
 
 $(call inherit-product, device/motorola/athene/full_athene.mk)
 
@@ -32,9 +32,7 @@ PRODUCT_PACKAGES += \
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := athene
-PRODUCT_NAME := xenonhd_athene
+PRODUCT_NAME := liquid_athene
 PRODUCT_BRAND := Motorola
 PRODUCT_MANUFACTURER := Motorola
 PRODUCT_RELEASE_NAME := athene
-
-PRODUCT_PROPERTY_OVERRIDES += ro.xenonhd.maintainer="Vachounet"
